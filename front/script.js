471a4a0e3a2ce1008c076d2afd3d1530ec66c8b6
Sahas Sharma <sahassharma19@gmail.com>
2024-02-17 14:11:36 -0600

    intial

---
+++ front/script.js
@@ -0,0 +1,16 @@
+// This script should be linked to your index.html file
+
+// Redirect to the Make an Appointment page
+document.getElementById('makeAppointmentBtn').addEventListener('click', function() {
+    window.location.href = 'make-appointment.html';
+});
+
+// Redirect to the Medication page
+document.getElementById('medicationBtn').addEventListener('click', function() {
+    window.location.href = 'medication.html';
+});
+
+// Redirect to the Enter Symptoms page
+document.getElementById('enterSymptomsBtn').addEventListener('click', function() {
+    window.location.href = 'enter-symptoms.html';
+});
